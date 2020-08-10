6d860c4e7bf3b16b965200ff8dbc4e8b60028070
aakashak2000 <47408219+aakashak2000@users.noreply.github.com>
2020-08-10 18:17:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods = ['POST'])
+@app.route("/", methods = ['GET', 'POST'])
 def upload_file():
     allScripts = []
     warn = '*This might take a few minutes'
